00c4320ea8484c4891419265014e1ecea937c69e
mac <james@qq.com>
2020-01-07 17:30:01 +0800

    no message

--- chatDemo/AppDelegate.swift
+++ chatDemo/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import TigaseSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,22 +15,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var client = XMPPClient();
+        // register modules
+                       let jid = BareJID("zqc1f7b4b49d4641e8b76ae3a59cd25a0f_uc@ul");
+        client.connectionConfiguration.setServerHost("hfuc.aitelian.cn");
+                       client.connectionConfiguration.setServerPort(5221);
+                       client.connectionConfiguration.setUserJID(jid);
+                       client.connectionConfiguration.setUserPassword("Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6InpxYzFmN2I0YjQ5ZDQ2NDFlOGI3NmFlM2E1OWNkMjVhMGYiLCJ0eXBlIjoidXNlciIsImV4cCI6MTU3OTU4OTcwMSwiaWF0IjoxNTc4MjkzNzAxfQ.oGpgG8Wpv6BmMqLK5-Xd-_JNA77Fpy6OKBf4zgZcgWM");
+   // create and register event handler
+   class EventBusHandler: EventHandler {
+      init() {
+      }
+   
+      func handle(event: Event) {
+          print("event bus handler got event = ", event);
+          switch event {
+          case is SessionEstablishmentModule.SessionEstablishmentSuccessEvent:
+              print("successfully connected to server and authenticated!");
+          case is RosterModule.ItemUpdatedEvent:
+              print("roster item updated");
+          case is PresenceModule.ContactPresenceChanged:
+              print("received presence change event");
+          case is MessageModule.ChatCreatedEvent:
+              print("chat was created");
+          case is MessageModule.MessageReceivedEvent:
+              print("received message");
+          default:
+              // here will enter other events if this handler will be registered for any other events
+              break;
+          }
+      }
+   }
+
+   let eventHandler = EventBusHandler();
+        client.context.eventBus.register(handler: eventHandler, for: SessionEstablishmentModule.SessionEstablishmentSuccessEvent.TYPE, RosterModule.ItemUpdatedEvent.TYPE, PresenceModule.ContactPresenceChanged.TYPE, MessageModule.MessageReceivedEvent.TYPE, MessageModule.ChatCreatedEvent.TYPE);
+        print("Connecting to server..")
+        client.modulesManager.register(AuthModule());
+               client.modulesManager.register(StreamFeaturesModule());
+               client.modulesManager.register(SaslModule());
+               client.modulesManager.register(ResourceBinderModule());
+               client.modulesManager.register(SessionEstablishmentModule());
+
+               print("Registering module for handling presences..");
+               client.modulesManager.register(PresenceModule());
+               print("Registering module for handling messages..");
+                client.modulesManager.register(MessageModule());
+               print("Registering module for handling MUC...");
+               client.modulesManager.register(MucModule());
+                      client.login();
+                      print("Started async processing..");
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+  
 
 
 }
--- chatDemo/ViewController.swift
+++ chatDemo/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
-
+import TigaseSwift
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var client = XMPPClient();
+     
+                let jid = BareJID("sender@domain.com");
+                client.connectionConfiguration.setUserJID(jid);
+                client.connectionConfiguration.setUserPassword("Pa$$w0rd");
+               print("Connecting to server..")
+               client.login();
+               print("Started async processing..");
     }
 
+//    func registerModules() {
+//        print("Registering modules required for authentication and session establishment");
+//        _ = client.modulesManager.register(AuthModule());
+//        _ = client.modulesManager.register(StreamFeaturesModule());
+//        _ = client.modulesManager.register(SaslModule());
+//        _ = client.modulesManager.register(ResourceBinderModule());
+//        _ = client.modulesManager.register(SessionEstablishmentModule());
+//
+//        print("Registering module for handling presences..");
+//        _ = client.modulesManager.register(PresenceModule());
+//        print("Registering module for handling messages..");
+//        _ = client.modulesManager.register(MessageModule());
+//    }
+
+//    func setCredentials(userJID: String, password: String) {
+//        let jid = BareJID(userJID);
+//        client.connectionConfiguration.setUserJID(jid);
+//        client.connectionConfiguration.setUserPassword(password);
+//    }
+//
+//    /// Processing received events
+//    func handle(event: Event) {
+//        switch (event) {
+//        case is SessionEstablishmentModule.SessionEstablishmentSuccessEvent:
+//            sessionEstablished();
+//        case is SocketConnector.DisconnectedEvent:
+//            print("Client is disconnected.");
+//        case let cpc as PresenceModule.ContactPresenceChanged:
+//            contactPresenceChanged(cpc);
+//        case let mr as MessageModule.MessageReceivedEvent:
+//            messageReceived(mr);
+//        default:
+//            print("unsupported event", event);
+//        }
+//    }
+//
+//    /// Called when session is established
+//    func sessionEstablished() {
+//        print("Now we are connected to server and session is ready..");
+//
+//        let presenceModule: PresenceModule = client.modulesManager.getModule(PresenceModule.ID)!;
+//        print("Setting presence to DND...");
+//        presenceModule.setPresence(show: Presence.Show.dnd, status: "Do not distrub me!", priority: 2);
+//    }
+//
+//    func contactPresenceChanged(_ cpc: PresenceModule.ContactPresenceChanged) {
+//        print("We got notified that", cpc.presence.from, "changed presence to", cpc.presence.show);
+//    }
+//
+//    func messageReceived(_ mr: MessageModule.MessageReceivedEvent) {
+//        print("Received new message from", mr.message.from, "with text", mr.message.body);
+//
+//        let messageModule: MessageModule = client.modulesManager.getModule(MessageModule.ID)!;
+//        print("Creating chat instance if it was not received..");
+//        let chat = mr.chat ?? messageModule.createChat(with: mr.message.from!);
+//        print("Sending response..");
+//        _ = messageModule.sendMessage(in: chat!, body: "Message in response to: " + (mr.message.body ?? ""));
+//    }
 
 }
 
