c49534a229a3f902ee44a3e06e78d05881bb3a8d
mac <james@qq.com>
2020-03-01 20:18:47 +0800

    update

---
+++ SignalProtocol.framework/Headers/setup.h
@@ -0,0 +1,21 @@
+//
+//  setup.h
+//  libsignal-protocol-swift iOS
+//
+//  Created by User on 15.02.18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+#ifndef setup_h
+#define setup_h
+
+
+
+
+
+
+
+
+char* encryptMsgMethod(const char* currentUser,const char* receiverName, const char* orignalMsg);
+
+#endif /* setup_h */
